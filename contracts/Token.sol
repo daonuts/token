12eeca575eda16d90d2a3df12658ed69116acb36
Carl Larson <cslarson@gmail.com>
2019-10-31 08:36:47 -0400

    initial commit

---
+++ contracts/Token.sol
@@ -0,0 +1,227 @@
+pragma solidity ^0.4.24;
+
+/* import "./Controlled.sol"; */
+/* import "./ITokenController.sol"; */
+import "@aragon/apps-shared-minime/contracts/MiniMeToken.sol";
+import "@aragon/apps-shared-minime/contracts/ITokenController.sol";
+import "@aragon/os/contracts/lib/math/SafeMath.sol";
+
+pragma solidity ^0.4.24;
+
+contract Token is Controlled {
+    using SafeMath for uint256;
+
+    mapping (address => uint256) private _balances;
+    mapping (address => mapping (address => uint256)) private _allowed;
+    uint256 private _totalSupply;
+
+    string public name;
+    uint8 public decimals;
+    string public symbol;
+    bool public transfersEnabled;
+
+    string private constant ERROR_TRANSFERS_DISABLED = "ERROR_TRANSFERS_DISABLED";
+    string private constant ERROR_TOKEN = "TOKEN";
+    string private constant ERROR_PERMISSION = "PERMISSION";
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _amount);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _amount);
+
+    constructor(string _name, uint8 _decimals, string _symbol, bool _transfersEnabled) public {
+        name = _name;
+        decimals = _decimals;
+        symbol = _symbol;
+        transfersEnabled = _transfersEnabled;
+    }
+
+    /// @notice The fallback function: If the contract's controller has not been
+    ///  set to 0, then the `proxyPayment` method is called which relays the
+    ///  ether and creates tokens as described in the token controller contract
+    function () external payable {
+        require(isContract(controller), ERROR_PERMISSION);
+        require(ITokenController(controller).proxyPayment.value(msg.value)(msg.sender) == true, ERROR_TOKEN);
+    }
+
+    /**
+     * @dev Modifier to make a function callable only when caller is controller or transfers are enabled.
+     */
+    modifier transferable() {
+        require(msg.sender == controller || transfersEnabled, ERROR_TRANSFERS_DISABLED);
+        _;
+    }
+
+    /**
+     * @dev Total number of tokens in existence
+     */
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev Gets the balance of the specified address.
+     * @param owner The address to query the balance of.
+     * @return A uint256 representing the amount owned by the passed address.
+     */
+    function balanceOf(address owner) public view returns (uint256) {
+        return _balances[owner];
+    }
+
+    /**
+     * @dev Function to check the amount of tokens that an owner allowed to a spender.
+     * @param owner address The address which owns the funds.
+     * @param spender address The address which will spend the funds.
+     * @return A uint256 specifying the amount of tokens still available for the spender.
+     */
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowed[owner][spender];
+    }
+
+    /**
+     * @dev Function to mint tokens
+     * @param _to The address that will receive the minted tokens.
+     * @param _amount The amount of tokens to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function generateTokens(address _to, uint256 _amount) public onlyController returns (bool) {
+        _mint(_to, _amount);
+        return true;
+    }
+
+    /**
+     * @dev Burns a specific amount of tokens from the target address
+     * @param _from address The account whose tokens will be burned.
+     * @param _amount uint256 The amount of token to be burned.
+     */
+    function destroyTokens(address _from, uint256 _amount) public onlyController returns (bool) {
+        _burn(_from, _amount);
+        return true;
+    }
+
+    /// @notice Enables token holders to transfer their tokens freely if true
+    /// @param _transfersEnabled True if transfers are allowed
+    function enableTransfers(bool _transfersEnabled) public onlyController {
+        transfersEnabled = _transfersEnabled;
+    }
+
+    /**
+     * @dev Transfer token to a specified address
+     * @param to The address to transfer to.
+     * @param value The amount to be transferred.
+     */
+    function transfer(address to, uint256 value) public transferable returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+
+    /**
+     * @dev Transfer tokens from one address to another.
+     * Note that while this function emits an Approval event, this is not required as per the specification,
+     * and other compliant implementations may not emit the event.
+     * @param from address The address which you want to send tokens from
+     * @param to address The address which you want to transfer to
+     * @param value uint256 the amount of tokens to be transferred
+     */
+    function transferFrom(address from, address to, uint256 value) public transferable returns (bool) {
+        _transfer(from, to, value);
+        if(msg.sender != controller) {
+          _approve(from, msg.sender, _allowed[from][msg.sender].sub(value));
+        }
+        return true;
+    }
+
+    /**
+     * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+     * Beware that changing an allowance with this method brings the risk that someone may use both the old
+     * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+     * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     * @param spender The address which will spend the funds.
+     * @param value The amount of tokens to be spent.
+     */
+    function approve(address spender, uint256 value) public transferable returns (bool) {
+        _approve(msg.sender, spender, value);
+        return true;
+    }
+
+    /**
+     * @dev Transfer token for a specified addresses
+     * @param _from The address to transfer from.
+     * @param _to The address to transfer to.
+     * @param _amount The amount to be transferred.
+     */
+    function _transfer(address _from, address _to, uint256 _amount) internal {
+        // Alerts the token controller of the transfer
+        if (isContract(controller)) {
+            require(ITokenController(controller).onTransfer(_from, _to, _amount) == true, ERROR_TOKEN);
+        }
+
+        require(_to != address(0), ERROR_PERMISSION);
+
+        _balances[_from] = _balances[_from].sub(_amount);
+        _balances[_to] = _balances[_to].add(_amount);
+        emit Transfer(_from, _to, _amount);
+    }
+
+    /**
+     * @dev Approve an address to spend another addresses' tokens.
+     * @param _owner The address that owns the tokens.
+     * @param _spender The address that will spend the tokens.
+     * @param _amount The number of tokens that can be spent.
+     */
+    function _approve(address _owner, address _spender, uint256 _amount) internal {
+        // Alerts the token controller of the approve function call
+        if (isContract(controller)) {
+            require(ITokenController(controller).onApprove(_owner, _spender, _amount) == true, ERROR_TOKEN);
+        }
+
+        require(_spender != address(0), ERROR_PERMISSION);
+        require(_owner != address(0), ERROR_PERMISSION);
+
+        _allowed[_owner][_spender] = _amount;
+        emit Approval(_owner, _spender, _amount);
+    }
+
+    /**
+     * @dev Internal function that mints an amount of the token and assigns it to
+     * an account. This encapsulates the modification of balances such that the
+     * proper events are emitted.
+     * @param account The account that will receive the created tokens.
+     * @param value The amount that will be created.
+     */
+    function _mint(address account, uint256 value) internal {
+        require(account != address(0), ERROR_PERMISSION);
+
+        _totalSupply = _totalSupply.add(value);
+        _balances[account] = _balances[account].add(value);
+        emit Transfer(address(0), account, value);
+    }
+
+    /**
+     * @dev Internal function that burns an amount of the token of a given
+     * account.
+     * @param account The account whose tokens will be burnt.
+     * @param value The amount that will be burnt.
+     */
+    function _burn(address account, uint256 value) internal {
+        require(account != address(0), ERROR_PERMISSION);
+
+        _totalSupply = _totalSupply.sub(value);
+        _balances[account] = _balances[account].sub(value);
+        emit Transfer(account, address(0), value);
+    }
+
+    /// @dev Internal function to determine if an address is a contract
+    /// @param _addr The address being queried
+    /// @return True if `_addr` is a contract
+    function isContract(address _addr) internal view returns(bool) {
+        uint size;
+        if (_addr == 0)
+            return false;
+
+        assembly {
+            size := extcodesize(_addr)
+        }
+
+        return size>0;
+    }
+}
---
+++ test/token.js
@@ -0,0 +1,98 @@
+const Token = artifacts.require("Token");
+const { assertRevert } = require('@aragon/test-helpers/assertThrow')
+
+contract('Token', (accounts) => {
+
+  let token;
+  let amount = 10000;
+
+  context('transferable token', async () => {
+
+    beforeEach(async () => {
+        token = await Token.new('n', 0, 'n', true)
+    })
+
+    it('first account is controller', async () => {
+
+      let controller = await token.controller()
+
+      assert.equal(controller, accounts[0], "token controller wasn't first account");
+    });
+
+    it('controller can mint', async () => {
+      await token.generateTokens(accounts[0], amount)
+      let supply = await token.totalSupply()
+      let balance = await token.balanceOf(accounts[0])
+
+      assert.equal(supply.valueOf(), amount, "10000 wasn't the total supply");
+      assert.equal(balance.valueOf(), amount, "10000 wasn't in the first account");
+    });
+
+    it('non-controller cannot mint', async () => {
+      return assertRevert(async () => {
+        await token.generateTokens(accounts[1], amount, {from: accounts[1]})
+      })
+    });
+
+    it('normal transfer succeeds', async () => {
+      let moveAmount = 5000;
+
+      await token.generateTokens(accounts[0], amount)
+      await token.transfer(accounts[1], moveAmount, {from: accounts[0]})
+      await token.transfer(accounts[2], moveAmount, {from: accounts[1]})
+      let balance1 = await token.balanceOf(accounts[1])
+      let balance2 = await token.balanceOf(accounts[2])
+
+      assert.equal(balance1.valueOf(), 0, "0 wasn't in the second account");
+      assert.equal(balance2.valueOf(), moveAmount, "5000 wasn't in the third account");
+    });
+
+    it('Should allow and transfer tokens from address 2 to address 1 allowed to 3', async () => {
+      await token.generateTokens(accounts[2], amount)
+
+      await token.approve(accounts[3], 2, { from: accounts[2] });
+      const allowed = await token.allowance(accounts[2], accounts[3]);
+      assert.equal(allowed, 2);
+
+      await token.transferFrom(accounts[2], accounts[1], 1, { from: accounts[3] });
+
+      const allowed2 = await token.allowance(accounts[2], accounts[3]);
+      assert.equal(allowed2, 1);
+    });
+
+  })
+
+  context('non-transferable token', async () => {
+
+    beforeEach(async () => {
+        token = await Token.new('n', 0, 'n', false)
+    })
+
+    it('controller can mint', async () => {
+      await token.generateTokens(accounts[0], amount)
+      let supply = await token.totalSupply()
+      let balance = await token.balanceOf(accounts[0])
+
+      assert.equal(supply.valueOf(), amount, "10000 wasn't the total supply");
+      assert.equal(balance.valueOf(), amount, "10000 wasn't in the first account");
+    });
+
+    it('controller can still transfer non-transferable', async () => {
+      await token.generateTokens(accounts[0], amount)
+      await token.transfer(accounts[1], amount, {from: accounts[0]})
+      let balance = await token.balanceOf(accounts[1])
+
+      assert.equal(balance.valueOf(), amount, "10000 wasn't the total supply");
+    });
+
+    it('normal transfer fails', async () => {
+      await token.generateTokens(accounts[1], amount)
+
+      return assertRevert(async () => {
+        await token.transfer(accounts[2], amount, {from: accounts[1]})
+      })
+    });
+
+  })
+
+});
